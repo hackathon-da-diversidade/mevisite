8a02538d62210b07917e1366e802baacb8192f04
jeanbauerc@thoughtworks.com <jbauer@thoughtworks.com>
2019-03-26 17:17:30 -0300

    Adiciona reach router para ter rotas acessíveis na aplicação

--- src/index.js
+++ src/index.js
@@ -1,10 +1,34 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { render } from "react-dom"
+import { Router, Link } from "@reach/router"
+import './App.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const App = () => {
+  let Home = () => <div>Home</div>
+  let Indique = () => <div>Indique um lar</div>
+  let Lares = () => <div>Lares</div>
+  let Quemsomos = () => <div>Quem Somos</div>
+
+  return (
+    <div>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="indique">Indique um lar</Link>
+        <Link to="lares">Lares</Link>
+        <Link to="quemsomos">Quem somos</Link>
+      </nav>
+      <Router>
+        <Home path="/" />
+        <Indique path="indique" />
+        <Lares path="lares" />
+        <Quemsomos path="quemsomos" />
+      </Router>
+    </div>
+  )
+}
+
+render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
